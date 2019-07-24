8c86056bb3eecc89cc1f41d66a064ad0e4ffef4c
ZoloMohan <mohanzolomon@gmail.com>
2019-07-24 21:23:09 +0530

    Created Images Folder & Button CSS Update

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./Hangman.css";
-import img0 from "./0.jpg";
-import img1 from "./1.jpg";
-import img2 from "./2.jpg";
-import img3 from "./3.jpg";
-import img4 from "./4.jpg";
-import img5 from "./5.jpg";
-import img6 from "./6.jpg";
+import img0 from "./images/0.jpg";
+import img1 from "./images/1.jpg";
+import img2 from "./images/2.jpg";
+import img3 from "./images/3.jpg";
+import img4 from "./images/4.jpg";
+import img5 from "./images/5.jpg";
+import img6 from "./images/6.jpg";
 
 class Hangman extends Component {
   /** by default, allow 6 guesses and use provided gallows images. */
